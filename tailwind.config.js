a2e3d9104e01bc69297c7889605046d2b499bbe5
zhenya <zalixdan@mail.ru>
2024-03-14 13:22:17 +0500

    first

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    colors: {
+      'main-green': '#305346',
+      'main-bg': '#F8C8C0',
+      'card-bg': '#FBDCD7',
+      'main-color-text': '#686767',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
